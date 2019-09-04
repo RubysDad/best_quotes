20818b66f0e38e0869696dc05375284c45699daf
Rubysdad <marktravismorales@gmail.com>
2019-09-04 15:18:49 -0500

    Created Quotes Controller and added autoloading config

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,12 @@
+class QuotesController < Tabitha::Controller
+  def romans_8_28
+    'And we know that God causes all things to work together for good
+    to those who love God, to those who are called according to His purpose.'
+  end
+
+  def philippians_4_6
+    'Be anxious for nothing, but in everything by prayer and supplication
+    with thanksgiving let your requests be made known to God. And the peace of God,
+    which surpasses all comprehension, will guard your hearts and your minds in Christ Jesus.'
+  end
+end
